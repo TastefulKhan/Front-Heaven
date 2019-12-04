10aa6043732fbe209bd4378c5117ca3704a3a781
TastefulKhan <marko.vasev@gmail.com>
2019-12-04 16:57:51 +0800

    Updates

---
+++ precache-manifest.8725fd09601fa516ea14d7caaa9bd2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a12f7ac1753cd88b551300a45f3422f",
+    "url": "/Front-Heaven/index.html"
+  },
+  {
+    "revision": "d845008827baa468634a",
+    "url": "/Front-Heaven/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ac9de50067d1ca4c995",
+    "url": "/Front-Heaven/static/js/2.c3868944.chunk.js"
+  },
+  {
+    "revision": "d845008827baa468634a",
+    "url": "/Front-Heaven/static/js/main.2736a640.chunk.js"
+  },
+  {
+    "revision": "39a7d7266278332f8e92",
+    "url": "/Front-Heaven/static/js/runtime-main.d04e6977.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Front-Heaven/static/media/logo.25bf045c.svg"
+  }
+]);
